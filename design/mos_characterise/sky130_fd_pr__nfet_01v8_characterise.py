7933465175659eeb47fae920dc86b40facca696f
yrrapt <yrrapt@gmail.com>
2020-09-26 16:15:25 +0200

    Inital commit of MOS characterisation scripts

---
+++ design/mos_characterise/sky130_fd_pr__nfet_01v8_characterise.py
@@ -0,0 +1,12 @@
+import SpiceInterface
+
+
+# create the object
+spice_interface_obj = SpiceInterface.SpiceInterface()
+
+# start OP parameter evaluation
+device = 'sky130_fd_pr__nfet_01v8'
+w = 1
+l = [0.15, 0.18, 0.25, 0.50, 1.00, 2.00, 4.00, 8.00]
+
+spice_interface_obj.measure_mos_op(device, w, l, vds=[0,1.8,3], vbs=[0,1.8,1])
