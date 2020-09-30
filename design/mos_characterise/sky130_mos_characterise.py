740ac27de9479d884d52ba7b28e4fbf50671dab0
yrrapt <yrrapt@gmail.com>
2020-09-30 19:34:31 +0200

    Updated MOS characterise to characterise all devices specified in YAML file

---
+++ design/mos_characterise/sky130_mos_characterise.py
@@ -0,0 +1,23 @@
+import yaml
+import SpiceInterface
+
+
+# create the object
+spice_interface_obj = SpiceInterface.SpiceInterface()
+
+# read in the device simulation parameters
+with open(r'devices.yaml') as file:
+    devices = yaml.full_load(file)
+
+
+# loop through each of the devices making the measurements
+for device in devices:
+
+    print('-'*150)
+    print('EXTRACTING OPERATING POINTS FOR DEVICE: ', device)
+    print('-'*150)
+    spice_interface_obj.measure_mos_op( device, 
+                                        devices[device]['w'], 
+                                        devices[device]['l'], 
+                                        vds=devices[device]['vds'],
+                                        vbs=devices[device]['vbs'])
