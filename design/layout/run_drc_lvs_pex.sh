782c2ae95ab346d1b1e739d553e09609bd60bb72
yrrapt <yrrapt@gmail.com>
2020-10-14 20:12:28 +0200

    Completed divider_cml layout and made improvements to DRC/LVS/PEX script

--- design/layout/run_drc_lvs_pex.sh
+++ design/layout/run_drc_lvs_pex.sh
@@ -18,25 +18,38 @@ echo "gds read ../../amsat_txrx_ic.gds
 load $1
 flatten drc_cell
 load drc_cell
-select top cell
-port makeall
 extract all
 ext2spice lvs
+ext2spice subcircuits off
 ext2spice -o drc_cell_lvs.spice
-ext2spice default
+select top cell
+port makeall
 ext2spice cthresh 0.01
 ext2spice rthresh 0.01
 ext2spice subcircuit on
 ext2spice ngspice
 ext2spice -o drc_cell_pex.spice" > magic_commands.tcl
 
+# remove the old LVS export
+rm -f drc_cell_lvs.spice
+
 # run magic
 magic magic_commands.tcl &
+
+# wait for new file to be generated
+printf "Waiting for LVS extracted netlist to be generated.."
+while [ ! -s drc_cell_lvs.spice ]
+    do
+    printf "."
+    sleep 0.25
+done
+echo " "
 
 # check if lvs reference file is present in folder
 if [ -f "$1.spice" ]; then
     echo "Starting LVS"
-    yes quit | netgen -noc lvs "$1".spice drc_cell_lvs.spice /usr/local/share/sky130A/libs.tech/netgen/sky130A_setup.tcl lvs_report.out
+    #yes quit | netgen -noc lvs "$1".spice drc_cell_lvs.spice /usr/local/share/sky130A/libs.tech/netgen/sky130A_setup.tcl lvs_report.out
+    netgen -batch lvs "$1".spice drc_cell_lvs.spice /usr/local/share/sky130A/libs.tech/netgen/sky130A_setup.tcl lvs_report.out -json
 else
     echo "No reference spice netlist ($1.spice) not doing LVS"
 fi
