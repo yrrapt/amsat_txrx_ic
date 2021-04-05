a6f5afafc1434973faf7ce439ad3c1fea50f6fe8
yrrapt <yrrapt@gmail.com>
2021-04-05 18:59:59 +0200

    Moved cells to development branch to start with clean build up

--- design/bandgap_core/verification/config.py
+++
@@ -1,8 +0,0 @@
-
-
-PVT:
-    corners         : ['tt', 'sf', 'ff', 'ss', 'fs', 'll', 'hh', 'hl', 'lh']
-    temperatures    : [-40, 27, 125]
-    vdd             : [1.62, 1.8, 1.98]
-
-
--- design/fractional_n_divider/spr.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ../../../openlane/openlane
-./run.sh /amsat_txrx_ic/design/fractional_n_divider
