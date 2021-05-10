98dcc2bba5a4120b75517a1e7199b72dfced3f25
yrrapt <server@wintermute.home>
2021-05-10 20:43:38 +0200

    Added pipelining for faster clocking

--- design/dac_digital_interface/dac_digital_interface.v
+++ design/dac_digital_interface/dac_digital_interface.v
@@ -6,11 +6,13 @@ module dac_digital_interface
         input                                       clk_i,
         input                                       rst_ni,
 
+        input                                       en_i,
         input                                       randomise_en_i,
-
         input  [INPUT_WIDTH-1:0]                    input_binary_i,
+
         output [2**THERMOMETER_WIDTH-1:0]           output_thermometer_o,
-        output [INPUT_WIDTH-THERMOMETER_WIDTH-1:0]  output_binary_o
+        output [INPUT_WIDTH-THERMOMETER_WIDTH-1:0]  output_binary_o,
+        output                                      en_o
     );
 
     localparam THERMOMETER_LENGTH = 2**THERMOMETER_WIDTH;
@@ -18,27 +20,40 @@ module dac_digital_interface
     localparam RANDOM_WIDTH = 2;
 
     // thermometer encoding signals
-    reg  [THERMOMETER_WIDTH-1:0]    input_thermometer_slice;
+    wire [THERMOMETER_WIDTH-1:0]    input_thermometer_slice;
+    reg  [BINARY_WIDTH-1:0]         input_binary_slice_q;
     reg  [BINARY_WIDTH-1:0]         input_binary_slice;
-    wire [THERMOMETER_LENGTH-1:0]   reversed_thermometer;
-    reg  [THERMOMETER_LENGTH-1:0]   thermometer_code;
+    reg  [THERMOMETER_LENGTH-1:0]   reversed_thermometer;
+    wire [THERMOMETER_LENGTH-1:0]   thermometer_code;
 
     // dynamic switching signals
     reg  [14:0]                     lfsr_q;
-    wire [RANDOM_WIDTH-1:0]         remap_control;
+    reg  [RANDOM_WIDTH-1:0]         remap_control;
     wire [RANDOM_WIDTH-1:0]         random_signal;
     reg  [THERMOMETER_LENGTH-1:0]   thermometer_randomised;
+
+    // Simply pass the enable signal straight through
+    assign en_o = en_i;
 
     //-------------------------------------------------------------------------------------------------------//
     // Create the thermometer coded outputs
     //-------------------------------------------------------------------------------------------------------//
 
-    // use a reversed thermometer to leverage the shift command.
+    // pick off the binary LSBs and delay to match the pipelined thermometer coding
     always @(posedge clk_i) begin
-        input_thermometer_slice <= input_binary_i[INPUT_WIDTH-1:INPUT_WIDTH-THERMOMETER_WIDTH];
-        input_binary_slice <= input_binary_i[BINARY_WIDTH-1:0];
+        input_binary_slice_q <= input_binary_i[BINARY_WIDTH-1:0];
+        input_binary_slice <= input_binary_slice;
     end
-    assign reversed_thermometer = input_thermometer_slice == 0 ? 0 : (2**THERMOMETER_LENGTH)-1 << (THERMOMETER_LENGTH-input_thermometer_slice);
+    
+    // use a reversed thermometer to leverage the shift command.
+    assign input_thermometer_slice = input_binary_i[INPUT_WIDTH-1:INPUT_WIDTH-THERMOMETER_WIDTH];
+    always @(posedge clk_i) begin
+        if (input_thermometer_slice == 0) begin
+            reversed_thermometer <= 0;
+        end else begin
+            reversed_thermometer <= (2**THERMOMETER_LENGTH)-1 << (THERMOMETER_LENGTH-input_thermometer_slice);
+        end
+    end
 
     // reverse the bit order of to create the output
     integer i;
@@ -62,16 +77,26 @@ module dac_digital_interface
     assign random_signal = lfsr_q[14:13];
 
     // enable the randomisation?
-    assign remap_control = randomise_en_i ? random_signal : 2'b00;
+    always @(negedge rst_ni, posedge clk_i) begin
+        if (!rst_ni) begin
+            remap_control <= 2'b00;
+        end else begin
+            if (randomise_en_i) begin
+                remap_control <= random_signal;
+            end else begin
+                remap_control <= 2'b00;
+            end
+        end
+    end
 
     // map outputs
-    always @* begin
+    always @(posedge clk_i) begin
         case(remap_control)
-            0       : thermometer_randomised = thermometer_code;
-            1       : thermometer_randomised = {thermometer_code[3*THERMOMETER_LENGTH/4-1:0], thermometer_code[THERMOMETER_LENGTH-1:3*THERMOMETER_LENGTH/4]};
-            2       : thermometer_randomised = {thermometer_code[2*THERMOMETER_LENGTH/4-1:0], thermometer_code[THERMOMETER_LENGTH-1:2*THERMOMETER_LENGTH/4]};
-            3       : thermometer_randomised = {thermometer_code[1*THERMOMETER_LENGTH/4-1:0], thermometer_code[THERMOMETER_LENGTH-1:1*THERMOMETER_LENGTH/4]};
-            default : thermometer_randomised = 'X;
+            0       : thermometer_randomised <= thermometer_code;
+            1       : thermometer_randomised <= {thermometer_code[3*THERMOMETER_LENGTH/4-1:0], thermometer_code[THERMOMETER_LENGTH-1:3*THERMOMETER_LENGTH/4]};
+            2       : thermometer_randomised <= {thermometer_code[2*THERMOMETER_LENGTH/4-1:0], thermometer_code[THERMOMETER_LENGTH-1:2*THERMOMETER_LENGTH/4]};
+            3       : thermometer_randomised <= {thermometer_code[1*THERMOMETER_LENGTH/4-1:0], thermometer_code[THERMOMETER_LENGTH-1:1*THERMOMETER_LENGTH/4]};
+            default : thermometer_randomised <= 'X;
         endcase
     end
 
